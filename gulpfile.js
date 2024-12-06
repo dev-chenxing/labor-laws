07a4a1def32e5db0800e26e0be796c7073f55935
dev-chenxing <chenxing.io@outlook.com>
2024-12-06 17:59:35 +0800

    custom filter

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,21 @@
-const { series, src, dest } = require("gulp");
+const { series, src, dest, watch } = require("gulp");
 const gulpPug = require("gulp-pug");
 const gulpStylus = require("gulp-stylus");
 
 const pug = (done) => {
-    src("./src/**/*.pug", { ignore: "./src/partials/**/*.pug" }).pipe(gulpPug()).pipe(dest("./dist"));
-    done();
+	src("./src/**/*.pug", { ignore: "./src/partials/**/*.pug" }).pipe(gulpPug({
+		filters:{
+			'law': function (text, options) {
+				return "hello world";
+			}
+		}
+	})).pipe(dest("./dist"));
+	done();
 };
 
 const stylus = (done) => {
-    src("./src/_stylus/*.styl").pipe(gulpStylus()).pipe(dest("./dist/assets/css/"));
-    done();
+	src("./src/_stylus/*.styl").pipe(gulpStylus()).pipe(dest("./dist/assets/css/"));
+	done();
 };
 
-exports.build = series(pug, stylus);
+exports.build = series(pug, stylus)
