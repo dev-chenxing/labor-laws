0a47d5d7a678ec25f2d2289f5011a442289aae0f
陈刑 <chenxing.io@outlook.com>
2024-12-06 22:36:45 +0800

    law

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,29 @@ const { series, src, dest } = require("gulp");
 const gulpPug = require("gulp-pug");
 const gulpStylus = require("gulp-stylus");
 
+function law(text, options) {
+    function parse(line) {
+        if (/^目\s*录$/.test(line)) return `<p class="text-center"><strong>${line}</strong></p>`;
+        else {
+            const index = line.search(/\s/);
+            if (index > 0) {
+                const label = line.slice(0, index);
+                if (/[章节]$/.test(label)) return `<p class="text-center"><strong>${line}</strong></p>`;
+                else if (/条$/.test(label)) return `<p><strong>${label}</strong> ${line.slice(index)}</p>`;
+                else return `<p>${line}</p>`;
+            } else return `<p>${line}</p>`;
+        }
+    }
+    const lines = text.split("\n").map((line) => parse(line.trim()));
+    return lines.join("");
+}
+
 const pug = (done) => {
     src("./src/**/*.pug", { ignore: "./src/partials/**/*.pug" })
         .pipe(
             gulpPug({
                 filters: {
-                    law: function (text, options) {
-                        return "hello world";
-                    },
+                    law: law,
                 },
             })
         )
