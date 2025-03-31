e736391e6b17ad8227cac631b390bc53d73f2fff
陈刑 <dev-chenxing.io@outlook.com>
2025-03-31 14:34:31 +0800

    Headings

--- src/index.js
+++ src/index.js
@@ -3,10 +3,20 @@ import { readFileSync, writeFileSync } from "fs";
 const filename = "./1-original.mdx";
 let mdx = readFileSync(filename, "utf-8");
 
-let lines = mdx.split("\n").map((line) => {
-    return line.trim();
+let title;
+let lines = mdx.split("\n").map((line, index) => {
+	line = line.trim();
+	if (index==0){
+		title = line;
+		line = `# ${line}`
+	} else {
+        let section = line.match(/^第(.*)条 /)
+        if (section) {
+            line = line.replace(section[0], `**${section[0].trim()}** `)
+        }
+		}
+	return line;
 });
-const title = lines[0];
 
 const frontmatter = `---
 layout: "@layouts/LawLayout.astro"
@@ -17,5 +27,5 @@ title: ${title}
 mdx = frontmatter + lines.join("\n");
 
 writeFileSync("./1.mdx", mdx, {
-    flag: "w",
+	flag: "w",
 });
